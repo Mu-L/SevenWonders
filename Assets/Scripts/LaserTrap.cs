02e48812362bdc797f9ac614a16efe4109c19343
zheng shuo MacBook <zs8861@gmail.com>
2020-09-20 17:55:43 +0800

    增加激光陷阱和箱子进洞关闭陷阱功能

---
+++ Assets/Scripts/LaserTrap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserTrap : MonoBehaviour
+{
+    public GameObject playerRespawn;
+    public Transform respawnPos;    
+
+    //private GameObject playerToDead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerToDead = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.BoxCollider2D")
+        {
+            Destroy(other.gameObject);
+            GameObject playerObj = Instantiate(playerRespawn, respawnPos.position, Quaternion.identity);
+            GameController.camFollow.target = playerObj.transform;
+        }
+    }
+}
